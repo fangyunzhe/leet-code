232c9b3695270c811746f4c92d0b655c2fac8b56
fangyunzhe <yunzhe.fang@gmail.com>
2020-06-08 10:49:19 +0800

    Initial commit

---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+/**
+ * @Description 链表节点类
+ * @Author fyz
+ * @Date 2020/6/8 10:36
+ **/
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
