90acc4b3a5d21aa2b6a73379df7f0b303c350419
fangyunzhe <752184244@qq.com>
2020-06-09 23:13:38 +0800

    快乐数

---
+++ src/slide_window/Q209_Min_Sub_Array_Len.java
@@ -0,0 +1,22 @@
+package slide_window;
+
+/**
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组，并返回其长度。如果不存在符合条件的连续子数组，返回 0。
+ * <p>
+ * 示例: 
+ * <p>
+ * 输入: s = 7, nums = [2,3,1,2,4,3]
+ * 输出: 2
+ * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ */
+public class Q209_Min_Sub_Array_Len {
+
+    public int minSubArrayLen(int s, int[] nums) {
+        int left = 0;
+        int right = 0;
+        return 0;
+    }
+}
---
+++ src/slow_fast_pointer/Q202_Is_Happy.java
@@ -0,0 +1,49 @@
+package slow_fast_pointer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+ * 如果 n 是快乐数就返回 True ；不是，则返回 False 。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：19
+ * 输出：true
+ * 解释：
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/happy-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q202_Is_Happy {
+
+    /**
+     * 由于可能出现死循环，所以将计算结果放入set中来检测死循环
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        set.add(n);
+        while (n != 1) {
+            int sum = 0;
+            String s = String.valueOf(n);
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                int num = Integer.parseInt(Character.toString(c));
+                sum = num * num + sum;
+            }
+            if (set.contains(sum)) {
+                return false;
+            } else {
+                set.add(sum);
+                n = sum;
+            }
+        }
+        return true;
+    }
+}
