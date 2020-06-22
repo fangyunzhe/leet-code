d45915fe92ef1a78e082fb6e8d039afb383e48bb
fangyunzhe <yunzhe.fang@gmail.com>
2020-06-22 14:42:14 +0800

    hasCycle

--- src/linkedlist/Q141_Has_Cycle.java
+++ src/linkedlist/Q141_Has_Cycle.java
@@ -1,17 +1,51 @@
 package linkedlist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 给定一个链表，判断链表中是否有环。
- *
+ * <p>
  * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/linked-list-cycle
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Q141_Has_Cycle {
 
+    /**
+     * 利用HashSet判断有没有重复的节点
+     */
     public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
         return false;
+    }
+
+    /**
+     * 利用快慢指针
+     * 如果链表中有环，则两个指针最终会相遇
+     */
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 }
