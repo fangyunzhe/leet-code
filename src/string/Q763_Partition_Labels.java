1a6a35c14d658f13beb44522b1596afc3040a5fa
fangyunzhe <yunzhe.fang@gmail.com>
2020-06-22 17:56:03 +0800

    划分字母区间

--- src/string/Q763_Partition_Labels.java
+++ src/string/Q763_Partition_Labels.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description
  * @Author fyz
@@ -20,10 +23,38 @@ package string;
  * <p>
  * S的长度在[1, 500]之间。
  * S只包含小写字母 'a' 到 'z' 。
- * 通过次数12,097提交次数16,826
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/partition-labels
  **/
 public class Q763_Partition_Labels {
+
+    public List<Integer> partitionLabels(String S) {
+        // 用于存放char最后一次出现的下标
+        int[] hash = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            hash[S.charAt(i) - 'a'] = i;
+        }
+        // start,end用于存放当前区间的首尾
+        int start = 0;
+        int end = 0;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < S.length(); i++) {
+            // 取最大值为了使end确定后保持不变
+            end = Math.max(hash[S.charAt(i) - 'a'], end);
+            // 遍历到char最后出现到位置，重新开始找下一个区间
+            if (end == i) {
+                res.add(i - start + 1);
+                start = i + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new Q763_Partition_Labels().partitionLabels("ababcbacadefegdehijhklij");
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+    }
 }
