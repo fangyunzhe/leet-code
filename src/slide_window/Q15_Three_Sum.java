2203db7aacc00b4b61f4b84bee2e5c5cff601814
fangyunzhe <yunzhe.fang@gmail.com>
2020-06-29 11:32:12 +0800

    三数之和

--- src/slide_window/Q15_Three_Sum.java
+++ src/slide_window/Q15_Three_Sum.java
@@ -1,5 +1,7 @@
 package slide_window;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,7 +23,51 @@ import java.util.List;
  **/
 public class Q15_Three_Sum {
     public List<List<Integer>> threeSum(int[] nums) {
-        //todo
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 第一个数大于0，和肯定大于0
+            if (nums[i] > 0) {
+                break;
+            }
+            // 避免出现重复解
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    right--;
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, -1, -1, -1, 3, 3, 3};
+        List<List<Integer>> list = new Q15_Three_Sum().threeSum(nums);
+        for (List<Integer> integers : list) {
+            for (Integer integer : integers) {
+                System.out.print(integer + " ");
+            }
+        }
     }
 }
